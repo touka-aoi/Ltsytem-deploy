6d31b3e25dd6f441708287fb1fd118190ff81dc1
touka_aoi <toukaseisei@gmail.com>
2023-01-27 18:22:04 +0900

    only use supabase client
    supabaseクライアントのみ起動

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../app.css';
+
+	import { supabase } from '$lib/databaseClient/supabaseClient';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	import { userState } from "$lib/store/userState";
+
+	// import Header from '../lib/Header.svelte';
+	// import Fotter from '../lib/Fotter.svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+		
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
+</script>
+
+<!-- <Header /> -->
+<slot />
+<!-- <Fotter /> -->
