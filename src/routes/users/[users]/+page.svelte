b073e35d44c96a1d8f3f88537371a58940e484ca
touka_aoi_desktop <toukaseisei@gmail.com>
2023-02-11 00:54:54 +0900

    ユーザーページ過去の参加LTを表示

--- src/routes/users/[users]/+page.svelte
+++ src/routes/users/[users]/+page.svelte
@@ -2,93 +2,34 @@
 	import Logout from '$lib/Login/Logout.svelte';
 	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
-	import { Account } from '$lib/AccountsFacade';
-	import { base } from '$app/paths';
+	import UserInfo from "./Userinfo.svelte"
+	import UserLtInformation from './UserLtInformation.svelte';
+	import PastUserLtInformation from './PastUserLtInformation.svelte';
 
 	// サーバー情報
 	export let data: PageData;
 
-	const user = data.user;
-	const avatarUrl = data.avatarUrl;
+	const userName = data.user;
 	const LtData = data.LtData;
 	const isUser = data.isUser;
-
-	let avatarData: string | undefined = undefined;
-	let size = 10;
-
+	$: speakerReserveData = LtData.speaker.reserve
+	$: speakerEndData = LtData.speaker.end
 	onMount(async () => {
-		// 画像ダウンロード
-		if (avatarUrl) {
-			const account = new Account();
-			const { fileUrl, error } = await account.avatarRequest.downloadAvatar(avatarUrl);
-			avatarData = fileUrl;
-		}
 	});
 </script>
 
 <div class="flex flex-col md:flex-row justify-center gap-10 my-10">
 	<!-- userInfo -->
-	<div class="flex flex-col items-center">
-		<div style="height: {size}em; width: {size}em;" class="overflow-hidden rounded-full">
-			{#if avatarData}
-				<img src={avatarData} alt={avatarData ? 'Avatar' : 'No image'} />
-			{:else}
-				<div class="bg-slate-100 rounded-full flex flex-col justify-center items-center" style="height: {size}em; width: {size}em;" />
-			{/if}
-		</div>
-		<div class="mt-5">
-			<p class="text-xl font-bold">{user}</p>
-		</div>
-	</div>
-
+	<UserInfo userName={userName} size={12}></UserInfo>
 	<!-- LT info -->
 	<div class="px-3">
 		<!-- Latest LT -->
 		<div class = "min-w-[70vw]">
-			<h2 class="text-2xl mb-3">参加予定LT</h2>
+			<h2 class="text-2xl pb-4">参加予定LT</h2>
 			<!-- LTデータが存在する -->
-			{#if LtData.speaker.reserve.length != 0}
-				{#each LtData.speaker.reserve as data}
-					<div class="bg-slate-300 rounded-sm px-5 py-4">
-						<div class="mb-2 gap-4">
-							<a href="{base}/lts/{data.Ltid}" class="flex flex-col gap-1">
-								<div class="flex gap-2">
-									<p class=" bg-slate-100 px-3 rounded-full w-fit bottom-2 border-2 h-fit">
-										{data.assginMem} / {data.maxMem}
-									</p>
-									<p>{data.holdDay} {data.holdHour}</p>
-								</div>
-								<div>
-									<p class="text-xl">{data.Ltname}</p>
-								</div>
-							</a>
-						</div>
-						<div class="flex w-full">
-							<div class="bg-slate-100 w-full rounded-sm p-2">
-								<p class="text-xl">{data.LtTitle}</p>
-								<p class="mt-2">{data.LtComment}</p>
-								<div class="flex flex-col my-2">
-									{#if data.LtLink?.length == 0}
-										<p class="text-red-500">スライドURLが入力されていません</p>
-									{:else}
-										<a href={data.LtLink}>
-											<img class="max-w-[18px]" src="/link.svg" alt="スライドリンク" />
-										</a>
-									{/if}
-								</div>
-							</div>
-						</div>
-						{#if isUser}
-							<div class="flex gap-2 justify-between	mt-4">
-								<a href="/lts/{data.Ltid}/register" class="w-fit">
-									<button class="minibtn bg-yellow-200">修正する</button>
-								</a>
-								<a href={data.holdPlace} class="w-fit">
-									<button class="minibtn bg-slate-100">開催場所へ移動</button>
-								</a>
-							</div>
-						{/if}
-					</div>
+			{#if speakerReserveData.length != 0}
+				{#each speakerReserveData as data}
+					<UserLtInformation LtData={data} canRevise={isUser} />
 				{/each}
 			{:else}
 				<div class="bg-slate-300 rounded-sm min-h-[190px] flex flex-col justify-center items-center">
@@ -115,31 +56,19 @@
 				{/if}
 			</div> -->
 		<!-- Past LT -->
-		<!-- <div>
-				<p class = "text-2xl mb-4">参加したLT</p>
-				{#each LtData.speaker.end as data}
-					{data.Ltid}
-					{data.LtTitle}
-					{data.holdDay}
-					{data.holdHour}
-					{data.Ltname}
-				{/each}
-				{#each LtData.viewer.reserve as data}
-				{data.Ltname}
-				{data.holdDay}
-				{data.holdHour}
-				{data.holdPlace}
-				{data.Ltid}
+		<div>
+			<p class = "text-2xl pt-20 pb-4">参加したLT</p>
+			{#each speakerEndData as data}
+				<PastUserLtInformation LtData={data}/>
 			{/each}
-			</div> -->
-
+		</div>
 		{#if isUser}
-			<div class="flex justify-between my-4 items-end">
-				<a href="/users/preference">
-					<img src="/config.svg" class="max-w-[28px] min-w-[28px] " alt="preference" />
-				</a>
-				<Logout />
-			</div>
+		<div class="flex justify-between my-4 items-end">
+			<a href="/users/preference" class="bg-slate-100 rounded drop-shadow">
+				<img src="/config.svg" class="max-w-[28px] min-w-[28px]" alt="preference" />
+			</a>
+			<Logout />
+		</div>
 		{/if}
 	</div>
 </div>
