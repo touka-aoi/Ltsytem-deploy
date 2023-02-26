b66c536c856581489b930c48b2ba1bcc0060b030
touka_aoi_desktop <toukaseisei@gmail.com>
2023-02-26 17:20:19 +0900

    + 9 ?

--- src/routes/users/[users]/PastUserLtInformation.svelte
+++ src/routes/users/[users]/PastUserLtInformation.svelte
@@ -5,7 +5,7 @@
 	export let LtData: userSpeakHoldInfo;
 
 	function convertDate(date: Date) {
-		// date.setHours(date.getHours() - 9);
+		date.setHours(date.getHours() - 9);
 		const DateJp = date.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Tokyo' });
 		return DateJp;
 	}
